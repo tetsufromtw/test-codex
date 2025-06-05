8f0b2adebb0ac4af4b4468442fc2d9d25956300e
tetsufromtw <103303738+tetsufromtw@users.noreply.github.com>
2025-06-05 17:58:08 +0900

    Design showcase page

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,23 +1,28 @@
-import type { FC, ReactNode } from 'react'
+import type { FC, ReactNode } from 'react';
 
 interface Props {
-  children: ReactNode
-  variant: 'primary' | 'danger' | 'secondary'
-  onClick: () => void
+  children: ReactNode;
+  variant: 'primary' | 'danger' | 'secondary';
+  onClick: () => void;
+  disabled?: boolean;
 }
 
-const Button: FC<Props> = ({ children, variant, onClick }) => {
+const Button: FC<Props> = ({ children, variant, onClick, disabled }) => {
   const variantClass =
     variant === 'primary'
       ? 'button-primary'
       : variant === 'danger'
-      ? 'button-danger'
-      : 'button-secondary'
+        ? 'button-danger'
+        : 'button-secondary';
   return (
-    <button className={`button ${variantClass}`} onClick={onClick}>
+    <button
+      className={`button ${variantClass} ${disabled ? 'opacity-50 cursor-not-allowed' : ''}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
       {children}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
